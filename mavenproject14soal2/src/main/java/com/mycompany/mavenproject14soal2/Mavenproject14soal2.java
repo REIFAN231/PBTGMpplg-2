a11a7272db30b48335c2b1245b2fbd328221a6fa
REIFAN231 <reifansukmaputra@gmail.com>
2024-08-28 13:50:17 +0700

    Add files via upload

---
+++ mavenproject14soal2/src/main/java/com/mycompany/mavenproject14soal2/Mavenproject14soal2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject14soal2;
+
+/**
+ *
+ * @author user
+ */
+public class Mavenproject14soal2 {
+
+    public static void main(String[] args) {
+        AbstrakHewan objek = new burung();
+        objek.suara();
+    }
+}
---
+++ mavenproject14soal2/src/main/java/com/mycompany/mavenproject14soal2/burung.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject14soal2;
+
+/**
+ *
+ * @author user
+ */
+public class burung extends AbstrakHewan {
+    
+    @Override
+    public void suara() {
+        System.out.println("Suara Burung Mencicit");
+    }
+    
+}
