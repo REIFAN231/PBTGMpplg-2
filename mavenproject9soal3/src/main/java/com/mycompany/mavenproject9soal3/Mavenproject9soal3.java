88081accdcc065ff663f8ec4fd5f62c315b7eb65
REIFAN231 <reifansukmaputra@gmail.com>
2024-08-28 09:56:23 +0700

    Add files via upload

---
+++ mavenproject9soal3/src/main/java/com/mycompany/mavenproject9soal3/Mavenproject9soal3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject9soal3;
+
+/**
+ *
+ * @author user
+ */
+public class Mavenproject9soal3 {
+
+      public static void main(String[] args) {
+        int i = 1;
+        while (i < 6) {
+            System.out.println (i);
+            i++;
+
+      }
+    }
+}
