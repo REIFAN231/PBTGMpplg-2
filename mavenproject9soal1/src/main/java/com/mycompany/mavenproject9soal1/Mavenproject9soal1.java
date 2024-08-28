b141fd14b7d3fc992dbe170af0c5e91f5ff78d6c
REIFAN231 <reifansukmaputra@gmail.com>
2024-08-28 09:55:33 +0700

    Add files via upload

---
+++ mavenproject9soal1/src/main/java/com/mycompany/mavenproject9soal1/Mavenproject9soal1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject9soal1;
+
+/**
+ *
+ * @author user
+ */
+public class Mavenproject9soal1 {
+
+    public static void main(String[] args) {
+        
+        int i = 1; // Inisialisasi variabel untuk loop luar
+        
+        while (i <= 5) { // Loop luar untuk jumlah baris
+            int j = 1; // Inisialisasi variabel untuk loop dalam
+            
+            while (j <= i) { // Loop dalam untuk jumlah bintang di setiap baris
+                System.out.print("* ");
+                j++;
+            }
+            
+            System.out.println(); // Pindah ke baris baru setelah mencetak semua bintang di baris saat ini
+            i++;
+        }   
+ 
+    }
+}
