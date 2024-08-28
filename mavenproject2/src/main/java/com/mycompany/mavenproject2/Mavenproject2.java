d76f4a356dfa1f56387003a40cec0eb036773ba0
REIFAN231 <reifansukmaputra@gmail.com>
2024-08-28 08:03:29 +0700

    Add files via upload

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author user
+ */
+public class Mavenproject2 {
+
+    public static String Makanan;
+//Membuat variable tanpa Value/Nilai pada class 
+    public static final String Minuman = "Jus Jambu";
+
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+        Makanan = "Ayam Geprek ";//Mengisi Value pada variable Hobi
+        System.out.println("Makanan Favorit Saya " + Makanan);//Memanggil variable Hewan
+        System.out.println("Minuman Favorit Saya "
+                + Minuman);//Memanggil Variable Hewan
+
+    }
+}
